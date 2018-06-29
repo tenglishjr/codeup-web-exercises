5c8142b3361254fdadb3d3c0af48372d3adf461a
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-06-29 10:46:12 -0500

    Finally got jQuery working -__-

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,8 +1,37 @@
 'use strict';
 
-$(document).ready(function () {
+$(function () {
 
-    alert('DOM has loaded!')
+    /**======= jQuery Intro / Selectors ========**/
+
+        // $("#playButton").click(function () {
+        //
+        //     $("#playOutput").text($("#playInput").val());
+        //
+        // });
+        //
+        // $('.importante').on('mouseover', function () {
+        //     console.log('Paragraph is importante...');
+        //     $(this).css('background-color', 'yellow');
+        // });
+        //
+        // alert($('#corgi').text());
+
+        // $('.codeup').css('border', '1px solid red');
+
+        // $('li').css('font-size', '20px');
+        //
+        // $('h1, p, li').css('background-color', 'yellow');
+
+        // alert($('h1').text());
+
+    /**======= jQuery Events =======**/
+
+        /* Just for fun */
+        $('h1').click(function () {
+            setInterval(function (){$('p').toggle(500)},500);
+        })
+
 
 
 });
