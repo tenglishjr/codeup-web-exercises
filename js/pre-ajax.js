6542d233bfb87d2daa6c62ef691e634227e0f115
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-07-09 10:59:53 -0500

    Added slide animation to div, createList needs optimization

--- js/pre-ajax.js
+++ js/pre-ajax.js
@@ -25,24 +25,26 @@ $(document).ready(function () {
     ];
 
     // Creates and Populates <ul> in the #names-list div
-    function createList (arr) {
-        $(arr).each(function () {
+    function createList (arrObjs) {
+        $(arrObjs).each(function () {
             $('#names-list').append('<li>' + this.name + '</li>');
         });
     }
 
     // Assigns colors to each li
-    function colorListItems(arr){
+    function colorListItems(arrObjs){
         $('#names-list > li').each(function (i) {
-            $(this).css('color', arr[i].favColor);
+            $(this).css('color', arrObjs[i].favColor);
         });
     }
 
 
     // Button Click Event
     $('#add-names-btn').click(function () {
+        $('#names-list').css('display', 'none');
         createList(people);
         colorListItems(people);
+        $('#names-list').slideDown(500);
     });
     // add functionality to the button so that when it is clicked, an unordered list of names from the people array (included below) is added to the inner HTML of the div#names-list. For an extra bonus, make the font color of each list item map to that person's favColor value.
 
